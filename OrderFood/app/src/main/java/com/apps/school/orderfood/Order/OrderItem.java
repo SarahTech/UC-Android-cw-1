32ddac8ff2055ac72475b804fc934c3dc2e9c079
Sarah Alenzei <sarahhtech@gmail.com>
2022-09-04 16:37:53 +0300

    ss

---
+++ OrderFood/app/src/main/java/com/apps/school/orderfood/Order/OrderItem.java
@@ -0,0 +1,31 @@
+package com.apps.school.orderfood.Order;
+
+public class OrderItem {
+
+    private String name;
+    private double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
